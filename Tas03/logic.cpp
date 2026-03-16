0f30caccb8b09a2f8f6071bbb6f92e5b4b16905b
staslolpol <staspriho9@gmail.com>
2026-03-16 18:14:35 +0300

    task03 complite

---
+++ Tas03/logic.cpp
@@ -0,0 +1,12 @@
+#include "logic.h"
+int count_multiples_3(int number) {
+	return 3 * (1 + number) * number / 2;
+}
+
+int count_multiples_5(int number) {
+	return 5 * (1 + number) * number / 2;
+}
+
+int count_multiples_8(int number) {
+	return 8 * (1 + number) * number / 2;
+}
---
+++ Tas03/main.cpp
@@ -0,0 +1,11 @@
+#include "logic.h"
+
+int main() {
+
+	cout << "Is perfect number?: " <<
+		(is_perfect_number(6) ? "Yes" : "No") << endl;
+	cout << "Is perfect number?: " <<
+		(is_perfect_number(8) ? "Yes" : "No") << endl;
+
+	return 0;
+}
